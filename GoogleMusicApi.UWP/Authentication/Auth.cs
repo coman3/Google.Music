95924285692fc4e9a239cdb311714eb6e38458d1
Lachlan van der Velden <lvelden1@gmail.com>
2016-07-27 17:01:36 +1000

    Fixed Login. Ready to merge!

--- GoogleMusicApi.UWP/Authentication/Auth.cs
+++ GoogleMusicApi.UWP/Authentication/Auth.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Net.NetworkInformation;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
-using Windows.Storage.Streams;
 using GoogleMusicApi.UWP.Common;
 
 namespace GoogleMusicApi.UWP.Authentication
@@ -14,66 +15,61 @@ namespace GoogleMusicApi.UWP.Authentication
     // URL: https://github.com/simon-weber/gpsoauth/blob/master/gpsoauth/__init__.py
     public static class GoogleAuth
     {
-        public const string UserAgent = "Android-Music/" + version;
+        public const string UserAgent = "Android-Music/" + Version;
 
-        private const string authUrl = "https://android.clients.google.com/auth";
+        private const string AuthUrl = "https://android.clients.google.com/auth";
 
-        private const string b64Key = "AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6prwgi3" +
+        private const string B64Key = "AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6prwgi3" +
                                       "iJIZdodyhKZQrNWp5nKJ3srRXcUW+F1BD3baEVGcmEgqaLZUNBjm057pK" +
                                       "RI16kB0YppeGx5qIQ5QjKzsR8ETQbKLNWgRY0QRNVz34kMJR3P/LgHax/" +
                                       "6rmf5AAAAAwEAAQ==";
 
-        private const string version = "3019";
-        private const string playServicesVersion = "9246440";
+        private const string Version = "3120";
 
-        private static readonly RSAParameters androidKey = GoogleKeyUtils.KeyFromB64(b64Key);
+        private static readonly RSAParameters AndroidKey = GoogleKeyUtils.KeyFromB64(B64Key);
 
-        private static readonly HttpClient httpClient;
+        private static readonly HttpClient HttpClient;
 
         static GoogleAuth()
         {
-            httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
-            
+            HttpClient = new HttpClient();
+            HttpClient.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
         }
 
         public static string GetDeviceId()
         {
-            return DeviceInfo.CalculateMD5Hash(DeviceInfo.Instance.Id);
+            //CAN NOT BE LONGER THAN 16 CHARS. (i sware i didnt spent like 13 hours finding out why this wasnt working........)
+            return DeviceInfo.CalculateMD5Hash(DeviceInfo.Instance.Id).Substring(0, 15);
         }
         // perform_master_login
         public static async Task<Dictionary<string, string>> PerformMasterLoginAsync(UserDetails userDetails, LocaleDetails localeDetails,
-            string service = "ac2dm", int sdkVersion = 21)
+            string service = "ac2dm", int sdkVersion = 17)
         {
-            var signature = GoogleKeyUtils.CreateSignature(userDetails.Email, userDetails.Password, androidKey);
-            httpClient.DefaultRequestHeaders.Add("device", userDetails.AndroidId);
+            var signature = GoogleKeyUtils.CreateSignature(userDetails.Email, userDetails.Password, AndroidKey);
+
             var dict = new Dictionary<string, string>
             {
                 {"accountType", "HOSTED_OR_GOOGLE"},
                 {"Email", userDetails.Email},
                 {"has_permission", "1"},
-                {"system_partition", "1"},
                 {"add_account", "1"},
                 {"EncryptedPasswd", signature},
                 {"service", service},
                 {"source", "android"},
                 {"androidId", userDetails.AndroidId},
-                {"parentAndroidId", userDetails.AndroidId},
                 {"device_country", localeDetails.DeviceCountry},
                 {"operatorCountry", localeDetails.OperatorCountry},
-                {"google_play_services_version", playServicesVersion},
                 {"lang", localeDetails.Language},
                 {"sdk_version", sdkVersion.ToString()}
             };
 
-            return await performAuthRequestAsync(dict);
+            return await PerformAuthRequestAsync(dict);
         }
 
         // perform_oauth
         public static async Task<Dictionary<string, string>> PerformOAuthAsync(UserDetails userDetails, LocaleDetails localeDetails,
             string masterToken, string service, string app, string clientSig, int sdkVersion = 21)
         {
-            httpClient.DefaultRequestHeaders.Add("device", userDetails.AndroidId);
             var dict = new Dictionary<string, string>
             {
                 {"accountType", "HOSTED_OR_GOOGLE"},
@@ -81,28 +77,24 @@ namespace GoogleMusicApi.UWP.Authentication
                 {"has_permission", "1"},
                 {"EncryptedPasswd", masterToken},
                 {"service", service},
-                {"callerPkg", service},
                 {"source", "android"},
                 {"app", app},
                 {"client_sig", clientSig},
-                {"caller_sig", clientSig},
-                {"androidId", userDetails.AndroidId},
-                {"parentAndroidId", userDetails.AndroidId},
                 {"device_country", localeDetails.DeviceCountry},
                 {"operatorCountry", localeDetails.OperatorCountry},
                 {"lang", localeDetails.Language},
                 {"sdk_version", sdkVersion.ToString()}
             };
 
-            return await performAuthRequestAsync(dict);
+            return await PerformAuthRequestAsync(dict);
         }
 
         // _perform_auth_request
-        private static async Task<Dictionary<string, string>> performAuthRequestAsync(Dictionary<string, string> data)
+        private static async Task<Dictionary<string, string>> PerformAuthRequestAsync(Dictionary<string, string> data)
         {
             var content = new FormUrlEncodedContent(data);
 
-            var response = await httpClient.PostAsync(authUrl, content);
+            var response = await HttpClient.PostAsync(AuthUrl, content);
 
             response.EnsureSuccessStatusCode();
 
